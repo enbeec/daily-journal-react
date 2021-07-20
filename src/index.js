18f36b09f3f2fd1c4e597e0a36d391b96ac7ea9d
Val Currie <contact@valcuri.email>
2021-07-20 12:23:21 -0500

    created global style and deleted index.css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import { DailyJournal } from "./DailyJournal";
+import { createGlobalStyle } from "styled-components";
 import * as serviceWorker from "./serviceWorker";
+
+const GlobalStyle = createGlobalStyle`
+	body {
+	  margin: 0;
+	  padding: 2rem;
+	  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+		'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+		sans-serif;
+	  -webkit-font-smoothing: antialiased;
+	  -moz-osx-font-smoothing: grayscale;
+	}
+
+	code {
+	  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+		monospace;
+	}
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <DailyJournal />
   </React.StrictMode>,
   document.getElementById("root")
